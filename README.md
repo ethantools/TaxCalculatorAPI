856ec31c49ef621d72147070e5da50e209b23fbf
Kukatoo <89143732+ethantools@users.noreply.github.com>
2025-08-04 17:48:36 -0500

    Reorganization

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Stock Information API
 API for calculating net income or tax breakdown.
 
-The API is deployed live at: https://dividendapi.netlify.app/.netlify/functions/taxcalculator
+The API is deployed live at: https://netincomecalculator.netlify.app/.netlify/functions/taxcalculator
 
 # Example GET Request
+To get the net income you can make a GET request on:
+https://netincomecalculator.netlify.app/.netlify/functions/taxcalculator/netincome/:gross/:status/:state
+
 To get the tax breakdown you can make a GET request on: 
-https://dividendapi.netlify.app/.netlify/functions/taxcalculator/taxbreakdown/:gross/:status/:state
+https://netincomecalculator.netlify.app/.netlify/functions/taxcalculator/taxbreakdown/:gross/:status/:state
--- functions/taxcalculator.js
+++ functions/taxcalculator.js
@@ -4,6 +4,8 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 const { getLastFridayOrNonHolidayDate, dateToUnixTimestampPlusADay, dateToUnixTimestamp, formatDateToMatchApiArgument } = require('./helpers.js');
 
@@ -16,9 +18,9 @@ router.get('/', (req, res) => {
 })
 
 function getTaxData(year, state) {
-    const federal = JSON.parse(fs.readFileSync(path.join(__dirname, `../tax_data/${year}/federal.json`)));
-    const fica = JSON.parse(fs.readFileSync(path.join(__dirname, `../tax_data/${year}/fica.json`)));
-    const stateData = JSON.parse(fs.readFileSync(path.join(__dirname, `../tax_data/${year}/state/${state}.json`)));
+    const federal = JSON.parse(fs.readFileSync(path.join(__dirname, `../functions/tax_data/${year}/federal.json`)));
+    const fica = JSON.parse(fs.readFileSync(path.join(__dirname, `../functions/tax_data/${year}/fica.json`)));
+    const stateData = JSON.parse(fs.readFileSync(path.join(__dirname, `../functions/tax_data/${year}/state/${state}.json`)));
     return { federal, fica, state: stateData };
 }
 
